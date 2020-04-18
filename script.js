4c748bf9279d2a7b6c965639b820fba0375d1eb4
Áron Kovács <ironk@kovacs-nb.abbyy.ru>
2020-04-18 10:41:25 +0200

    fixed desktop wip: mobile

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //
-window.mobileCheck = function()  {
+window.mobileCheck = function () {
   let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
   console.log('Is mobile: ', check)
   return check;
 };
@@ -200,8 +200,8 @@ function init() {
   // html.mobileMenu.ferryLand = document.querySelector('.mobile-ferry—land');
 
   html.mobileMenu.wrapper = document.querySelector('.mobile-menu-wrapper');
-  html.mobileMenu.about = document.querySelector('.mobile-about');
-  html.mobileMenu.contact = document.querySelector('.mobile-contact');
+  // html.mobileMenu.about = document.querySelector('.mobile-about');
+  // html.mobileMenu.contact = document.querySelector('.mobile-contact');
 
 
   html.right = document.querySelector('.right');
@@ -226,8 +226,8 @@ function init() {
   // html.mobileMenu.oneness.addEventListener('click', () => { setHash('oneness') });
   // html.mobileMenu.ferryLand.addEventListener('click', () => { setHash('ferry-land') });
 
- html.mobileMenu.about.addEventListener('click', () => { setHash('about') });
- html.mobileMenu.contact.addEventListener('click', () => { setHash('contact') });
+  // html.mobileMenu.about.addEventListener('click', () => { setHash('about') });
+  // html.mobileMenu.contact.addEventListener('click', () => { setHash('contact') });
 
   if (currentHash !== newHashToSet) navigation(newHashToSet);
 }
@@ -268,17 +268,17 @@ function contact() {
 function generateGalleryElements(set) {
   let gallery = '';
   set.img.map(e => {
-   
-    gallery +=  window.mobileCheck()
-    ?`
+
+    gallery += window.mobileCheck()
+      ? `
       <div class="gallery-element" id="${e.title ? e.title : e.id}">
         <img src="${e.img}_mobil.jpg" alt="${e.title ? e.title : e.id}" class="gallery-img">
         <div class="gallery-title">
           ${e.title ? '<span>' + e.title + '</span>' : ''}
         </div>
       </div>
-  ` 
-  : `
+  `
+      : `
     <div class="gallery-element" id="${e.title ? e.title : e.id}">
       <img src="${e.img}_index.jpg" alt="${e.title ? e.title : e.id}" class="gallery-img">
       <div class="gallery-title">
@@ -399,16 +399,16 @@ function navigation(hash) {
   const toPage = hash[0] ? hash[0] : 'landing';
   // generatePath(toPage)
   let galleryId = hash[1] ? hash[1] : null;
-  if(window.mobileCheck()) galleryId = null;
+  if (window.mobileCheck()) galleryId = null;
   if (document.querySelector('.active')) document.querySelector('.active').classList.remove('active')
   console.log('navigation(hash) toPage: ', toPage);
 
 
   switch (toPage) {
     case 'landing':
-      if (window.mobileCheck()){
+      if (window.mobileCheck()) {
         gallery(toPage, mainSet, 1, false);
-      }else{
+      } else {
         landing();
       }
       break;
@@ -422,7 +422,7 @@ function navigation(hash) {
       if (!galleryId) {
         gallery(toPage, reflectionSet, 4)
       } else {
-        if(!window.mobileCheck()){
+        if (!window.mobileCheck()) {
           singleGallery(reflectionSet, galleryId)
         }
       }
@@ -433,7 +433,7 @@ function navigation(hash) {
       if (!galleryId) {
         gallery(toPage, epitomeSet, 2)
       } else {
-        if(!window.mobileCheck()){
+        if (!window.mobileCheck()) {
           singleGallery(epitomeSet, galleryId)
         }
       }
@@ -454,7 +454,7 @@ function navigation(hash) {
       if (!galleryId) {
         gallery(toPage, knotSet, 3)
       } else {
-        if(!window.mobileCheck()){
+        if (!window.mobileCheck()) {
           singleGallery(knotSet, galleryId)
         }
       }
@@ -465,7 +465,7 @@ function navigation(hash) {
       if (!galleryId) {
         gallery(toPage, onenessSet, 3)
       } else {
-        if(!window.mobileCheck()){
+        if (!window.mobileCheck()) {
           singleGallery(onenessSet, galleryId)
         }
       }
@@ -476,7 +476,7 @@ function navigation(hash) {
       if (!galleryId) {
         gallery(toPage, ferryLandSet, 4)
       } else {
-        if(!window.mobileCheck()){
+        if (!window.mobileCheck()) {
           singleGallery(ferryLandSet, galleryId)
         }
       }
@@ -496,18 +496,18 @@ function navigation(hash) {
       about()
       html.menu.classList.remove('hidden');
       html.mobileMenu.about.classList.add('active')
-      html.menu.about.classList.add('active')
+      // html.menu.about.classList.add('active')
       break;
     case 'contact':
       contact()
       html.menu.classList.remove('hidden');
       html.menu.contact.classList.add('active')
-      html.mobileMenu.contact.classList.add('active')
+      // html.mobileMenu.contact.classList.add('active')
       break;
   }
-  if (window.mobileCheck()){
+  if (window.mobileCheck()) {
     html.menu.style = "display:none"
-  } else{
+  } else {
     html.mobileMenu.wrapper.style = "display:none"
   }
 }
@@ -549,21 +549,20 @@ function hamburger() {
   }
 }
 
-function calculateImgH() {
-  const imgRatio = 0.8;
-  const maxW = 1400;
-  const maxH = 1120;
+function calculateImgW() {
+//   const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+//   const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  
+//   const imgRatio = 0.8;
+//   const maxW = 1000;
+//   const maxH = 100 * vh - 140;
+  
+//   const contW = document.querySelector('.caroussel').offsetWidth;
+//   const contH = document.querySelector('.caroussel').offsetHeight;
 
-  const contW = document.querySelector('.caroussel').offsetWidth;
-  const contH = document.querySelector('.caroussel').offsetHeight;
-
-  if (contW >= maxW && contH >= maxH) {
-    return [maxW, maxH]
-  } else if (maxW * imgRatio > contH) {
-    return [contH * (1 / imgRatio), contH]
-  } else {
-    return [contW, contW * imgRatio]
-  }
+// if(contW >= mainSet && contH >= maxH){
+//   return  maxW
+// }else if();
 }
 
 function getImgH() {
