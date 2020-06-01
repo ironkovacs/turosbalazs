f83dba7b74cbff7ee2db28460118a6e90a4719fa
Aron Kovacs <ironkovacs@gmail.com>
2020-06-01 23:04:30 +0200

    mobile menu hide

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 //
+
 window.mobileCheck = function () {
   let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  const width = (window.innerWidth > 0) ? window.innerWidth : screen.width
+  const height = (window.innerHeight > 0) ? window.innerHeight : screen.height
+  if (width >= 1024 && height >= 768) {
+    check = false
+  }
+  else {
+    (function (a) {
+      if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
+        check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+  }
+  console.log('Mobile view: ', check)
   return check;
 };
 // elements
 let html = {}
 let isHamburgerOpen = false;
 let currentHash = '';
+
 
 const components = {
   landing: './components/landing/landing.html',
@@ -179,6 +192,7 @@ function init() {
   html.main = document.querySelector('main');
   html.left = document.querySelector('.left');
   html.title = document.querySelector('.title');
+  html.titleMobile = document.querySelector('.title-mobile');
   html.menu = document.querySelector('.menu');
   html.menu.works = {};
   html.mobileMenu = {};
@@ -192,19 +206,7 @@ function init() {
 
   html.menu.about = document.querySelector('.nav-about');
   html.menu.contact = document.querySelector('.nav-contact');
-
-  // html.mobileMenu.hamburger = document.querySelector('.hamburger')
-  // html.mobileMenu.wrapper = document.querySelector('.mobile-menu-wrapper')
-  // html.mobileMenu.reflection = document.querySelector('.mobile-reflection');
-  // html.mobileMenu.epitome = document.querySelector('.mobile-epitome');
-  // html.mobileMenu.closer = document.querySelector('.mobile-closer');
-  // html.mobileMenu.knot = document.querySelector('.mobile-knot');
-  // html.mobileMenu.oneness = document.querySelector('.mobile-oneness');
-  // html.mobileMenu.ferryLand = document.querySelector('.mobile-ferry—land');
-
   html.mobileMenu.wrapper = document.querySelector('.mobile-menu-wrapper');
-  // html.mobileMenu.about = document.querySelector('.mobile-about');
-  // html.mobileMenu.contact = document.querySelector('.mobile-contact');
 
 
   html.right = document.querySelector('.right');
@@ -221,18 +223,64 @@ function init() {
   html.menu.about.addEventListener('click', () => { setHash('about') });
   html.menu.contact.addEventListener('click', () => { setHash('contact') });
 
-  // html.mobileMenu.hamburger.addEventListener('click', () => { hamburger() });
-  // html.mobileMenu.reflection.addEventListener('click', () => { setHash('perceptions') });
-  // html.mobileMenu.epitome.addEventListener('click', () => { setHash('epitome') });
-  // html.mobileMenu.closer.addEventListener('click', () => { setHash('closer') });
-  // html.mobileMenu.knot.addEventListener('click', () => { setHash('ephemeral-knot') });
-  // html.mobileMenu.oneness.addEventListener('click', () => { setHash('oneness') });
-  // html.mobileMenu.ferryLand.addEventListener('click', () => { setHash('ferry-land') });
-
-  // html.mobileMenu.about.addEventListener('click', () => { setHash('about') });
-  // html.mobileMenu.contact.addEventListener('click', () => { setHash('contact') });
-
   if (currentHash !== newHashToSet) navigation(newHashToSet);
+
+
+  function scrollDistance(callback, refresh) {
+    // Make sure a valid callback was provided
+    if (!callback || typeof callback !== 'function') return;
+
+    // Variables
+    var isScrolling, start, end, distance;
+
+    // Listen for scroll events
+    html.right.addEventListener('scroll', function (event) {
+
+      // Set starting position
+      if (!start) {
+        start = html.right.scrollTop;
+      }
+
+      // Clear our timeout throughout the scroll
+      window.clearTimeout(isScrolling);
+
+      // Set a timeout to run after scrolling ends
+      isScrolling = setTimeout(function () {
+
+        // Calculate distance
+        end = html.right.scrollTop;
+        distance = end - start;
+
+        // Run the callback
+        callback(distance, start, end);
+
+        // Reset calculations
+        start = null;
+        end = null;
+        distance = null;
+
+      }, refresh || 66);
+
+    }, false);
+
+  };
+  scrollDistance(function (distance) {
+    if (window.mobileCheck()) {
+      distance < 0 ? scrollUp(distance) : scrollDown(distance);
+    }
+  });
+  function scrollDown(distance) {
+    console.log('up', distance)
+    html.left.classList.add('closed')
+    document.querySelector('.menu-mobile').classList.add('closed')
+  }
+
+  function scrollUp(distance) {
+    console.log('down', distance)
+    html.left.classList.remove('closed')
+    document.querySelector('.menu-mobile').classList.remove('closed')
+
+  }
 }
 
 function landing() {
@@ -536,54 +584,16 @@ function rngMinMax(min, max) {
     return 2;
   }
 }
-
-function hamburger() {
-  // let menu = html.mobileMenu.wrapper;
-  menu.classList.remove('open');
-  menu.classList.remove('closed');
-  html.title.classList.remove('open');
-  html.title.classList.remove('closed');
-  if (isHamburgerOpen) {
-    menu.classList.add('closed')
-    html.title.classList.add('closed')
-    isHamburgerOpen = false
-  } else {
-    menu.classList.add('open')
-    html.title.classList.add('open')
-    isHamburgerOpen = true
+window.onorientationchange = function () {
+  var orientation = window.orientation;
+  switch (orientation) {
+    case 0:
+    case 90: window.location.reload()
+    case -90: window.location.reload();
+      break;
   }
-}
-
-// function calculateImgW() {
-//   const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-//   const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  
-//   const imgRatio = 0.8;
-//   const maxW = 900;
-//   const maxH = 100 * vh - 140;
-  
-//   const contW = document.querySelector('.caroussel').offsetWidth;
-//   const contH = document.querySelector('.caroussel').offsetHeight;
-
-
-
-// function getImgH() {
-//   return document.querySelector('.single-img').offsetHeight;
-// }
-
-// window.addEventListener('resize', setGalleryNavHeight)
-
-// function setGalleryNavHeight() {
-//   if (document.querySelector('.gallery-side')) {
-//     setInterval(() => {
-//       const _height = getImgH() / 2
-//       document.querySelector('.gallery-side').style = `height:${_height}px`
-//       console.log(_height)
-//     }, 30);
-
-//   }
-// }
+};
 
 window.addEventListener('hashchange', init);
-
 window.onload = () => { init() };
+
